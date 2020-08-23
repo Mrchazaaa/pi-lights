2a5190372630db35a2eee622dba2560d32ab62ce
mrchazaaa <charlie.howlett@live.co.uk>
2020-08-23 18:29:57 +0100

    minor service tweaks

--- run-lights.sh
+++ run-lights.sh
@@ -5,4 +5,4 @@
 # exec > /home/pi/workspace/lights/logs/${todaysDate}.log 
 # exec 2>&1
 
-node /home/pi/workspace/lights/server/src/index.js
+node /home/pi/workspace/lights/server/build/server/src/index.js
