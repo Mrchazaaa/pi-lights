bd7d63564999e5cdf9b9bd7da3b41b311ef6d509
mrchazaaa <charlie.howlett@live.co.uk>
2022-09-02 00:09:24 +0100

    Fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -5,24 +5,37 @@
 # exec > /home/pi/workspace/lights/logs/${todaysDate}.log
 # exec 2>&1
 
+echo "building client"
+
 npm install --prefix ./client
 
 npm run build --prefix ./client
+
+echo "building server"
 
 npm install --prefix ./server
 
 npm run build --prefix ./server
 
+echo "creating light control service"
+
 cp ./light-control.service /etc/systemd/system
 
 systemctl daemon-reload
+
+echo "restart light control service"
 
 systemctl stop light-control.service
 systemctl start light-control.service
 systemctl enable light-control.service
 
-docker rm loki-container
-docker rm promtail-container
+echo "killing loki container"
+docker kill loki-container
+
+echo "killing promtail container"
+docker kill promtail-container
+
+echo "spinning up containers"
 docker-compose up -d
 
 # sudo bash deploy.sh
